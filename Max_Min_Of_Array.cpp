f2296ef1bb6d56a42fa0efc2eb8ff7e411a2f694
khushi-rathore <khushirathore1520@gmail.com>
2024-01-05 18:41:49 +0530

    Adding file

---
+++ Max_Min_Of_Array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int getMax(int arr[], int size)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int getMin(int arr[], int size)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+int main()
+{
+    int size;
+    cin >> size;
+    int number[100];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> number[i];
+    }
+    cout << "The Maximum value is : " << getMax(number, size);
+    cout << "The Minimum value is : " << getMin(number, size);
+}
